f2bf87db111a5934c61b2cae22a578fb91a76916
CheyenneValor <web25cvalor@imaltuna.com>
2026-01-12 13:57:55 +0100

    Artikulua_cheyenne

--- Articulo/src/App.java
+++ Articulo/src/App.java
@@ -3,11 +3,11 @@ import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Author autorea = new Author("aitzol");
+        Author autorea = new Author("cheyenne");
         LocalDate gaur = LocalDate.now();
         Articulo a1 = new Articulo(autorea, Categoria.BACKEND, gaur, "Lehenengo Java Artikulua");
         System.out.println(a1.toString());
-        Articulo a2 = new Articulo(autorea, Categoria.BACKEND, gaur, "Lehenengo Java Artikulua");
-        System.out.println(a2.toString());
+        Articulo Cheyenne = new Articulo(autorea, Categoria.BACKEND, gaur, "Lehenengo Java Artikulua");
+        System.out.println(Cheyenne.toString());
     }
 }
