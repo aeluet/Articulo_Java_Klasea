cbc7524f0e0a5e036d2ae2e2d58064a833e53e1e
web25hagirre <web25haguirre@imaltuna.com>
2026-01-12 13:57:51 +0100

    Artikulua - Hodei

--- Articulo/src/App.java
+++ Articulo/src/App.java
@@ -3,11 +3,13 @@ import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Author autorea = new Author("aitzol");
+        Author autorea = new Author("hodei");
         LocalDate gaur = LocalDate.now();
         Articulo a1 = new Articulo(autorea, Categoria.BACKEND, gaur, "Lehenengo Java Artikulua");
         System.out.println(a1.toString());
         Articulo a2 = new Articulo(autorea, Categoria.BACKEND, gaur, "Lehenengo Java Artikulua");
         System.out.println(a2.toString());
+        Articulo hodei = new Articulo(autorea, Categoria.BACKEND, gaur, "Lehenengo Java Artikulua");
+        System.out.println(hodei.toString());
     }
 }
